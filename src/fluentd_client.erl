a02264a72c042c3dfef45b892050533f611e0102
ryanaudiencepi <ryanaudiencepi@users.noreply.github.com>
2013-10-24 01:46:30 +0800

    dont use macros for application name

--- src/fluentd_client.erl
+++ src/fluentd_client.erl
@@ -3,7 +3,6 @@
 -export([start_link/0]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 -record(state, {host, port, sock}).
--define(APPLICATION, fluentd).
 
 %%====================================================================
 %% API
@@ -53,7 +52,7 @@ get_env(Key) ->
     get_env(Key, undefined).
 
 get_env(Key, Default) ->
-    case application:get_env(?APPLICATION, Key) of
+    case application:get_env(fluentd, Key) of
         {ok, Value} -> Value;
         undefined -> Default
     end.
